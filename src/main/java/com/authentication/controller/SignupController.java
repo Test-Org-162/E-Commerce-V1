3cdffb42f7164287b1c56e3011ec8ef02104ad3d
biswajeet162 <biswajeet163@gmail.com>
2021-05-23 00:18:55 +0530

    Swagger Configuration File Added.

--- src/main/java/com/authentication/controller/SignupController.java
+++ src/main/java/com/authentication/controller/SignupController.java
@@ -2,15 +2,17 @@ package com.authentication.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(value = "/api")
 public class SignupController {
 
     @ResponseBody
-    @RequestMapping(value = "/signup",method = RequestMethod.GET)
+    @GetMapping( value = "/signup")
     public String signup(){
         return "do Signup";
     }
