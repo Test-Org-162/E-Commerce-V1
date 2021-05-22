681b74c898d807a2db7cee1831b1fadfdfe3217c
biswajeet162 <biswajeet163@gmail.com>
2021-05-22 23:48:33 +0530

    Updating

---
+++ src/main/java/com/authentication/controller/ControllerAdviceException.java
@@ -0,0 +1,7 @@
+package com.authentication.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdviceException {
+}
---
+++ src/main/java/com/authentication/exception/UserNotFound.java
@@ -0,0 +1,4 @@
+package com.authentication.exception;
+
+public class UserNotFound {
+}
